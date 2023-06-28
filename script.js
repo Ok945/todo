c92e14f20efad4f45ffdeac11b46ba2a56073071
Ok945 <omkendre5454@gmail.com>
2023-06-28 19:16:57 +0530

    trying to add info and basic info and about zest in different pages

--- script.js
+++ script.js
@@ -15,25 +15,25 @@ $(document).ready(function() {
                 $("option[value='" + selected + "']", $(this)).hide();
             }
         });
-        // var p1 = document.getElementById('Preference_1').value;
-        // var p2 = document.getElementById('Preference_2').value;
-        // var p3 = document.getElementById('Preference_3').value;
+        var p1 = document.getElementById('Preference_1').value;
+        var p2 = document.getElementById('Preference_2').value;
+        var p3 = document.getElementById('Preference_3').value;
 
-        // if(p1 == p2 || p1 == p3 || p2 == p3)
-        // {
-        //     alert("Please select different preferences!");
-        //     if(p1 == p2)
-        //     {
-        //         document.getElementById('Preference_2').value = 2;
-        //     }
-        //     if(p1 == p3)
-        //     {
-        //         document.getElementById('Preference_3').value = 3;
-        //     }
-        //     if(p2 == p3)
-        //     {
-        //         document.getElementById('Preference_3').value = 3;
-        //     }
-        // }
+        if(p1 == p2 || p1 == p3 || p2 == p3)
+        {
+            alert("Please select different preferences!");
+            if(p1 == p2)
+            {
+                document.getElementById('Preference_2').value = 2;
+            }
+            if(p1 == p3)
+            {
+                document.getElementById('Preference_3').value = 3;
+            }
+            if(p2 == p3)
+            {
+                document.getElementById('Preference_3').value = 3;
+            }
+        }
     });
 });
